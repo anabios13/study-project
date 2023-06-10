f3e59cbdbf5cbcdd4cdc528a448bce40abde5562
anabios13 <o.pinchykov@mail.ru>
2023-06-10 17:31:18 +0300

    Revert "was added JWTTokenEncoderProxy"
    This reverts commit 898714a7

--- src/main/java/by/anabios13/authorizationService/config/SecurityConfig.java
+++ src/main/java/by/anabios13/authorizationService/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package by.anabios13.authorizationService.config;
 
 import by.anabios13.authorizationService.filters.JWTFilter;
+import by.anabios13.authorizationService.repository.UserRepository;
+import by.anabios13.authorizationService.security.JWTUtil;
 import by.anabios13.authorizationService.services.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,17 +22,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
     private UserDetailsService userDetailsService;
-
+    private final UserRepository userRepository;
+    private final JWTFilter jwtFilter;
+    private final JWTUtil jwtUtil;
 
 
 
     @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
+    public SecurityConfig(UserDetailsService userDetailsService, UserRepository userRepository, JWTFilter jwtFilter, JWTUtil jwtUtil) {
         this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+        this.jwtFilter = jwtFilter;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,JWTFilter jwtFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()
@@ -48,11 +58,16 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager()
             throws Exception {
-        return new EncryptedPasswordAuthenticationManager(userDetailsService,getPasswordEncoder());
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new JWTTokenEncoderProxy();
+        return new BCryptPasswordEncoder();
     }
+
+
 }
